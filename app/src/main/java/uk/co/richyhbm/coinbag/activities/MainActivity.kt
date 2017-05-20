5ac5bfa751a0ec13d24e73254ff80039131b341f
RichyHBM <gitemail@richyhbm.co.uk>
2017-05-20 12:29:10 +0100

    Have splash screen initialise intro

--- app/src/main/java/uk/co/richyhbm/coinbag/activities/MainActivity.kt
+++ app/src/main/java/uk/co/richyhbm/coinbag/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package uk.co.richyhbm.coinbag.activities
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import uk.co.richyhbm.coinbag.R
@@ -8,13 +9,11 @@ import android.content.Intent
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        val i = Intent(this@MainActivity, WelcomeActivity::class.java)
-        startActivity(i)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {

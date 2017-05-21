d0a54d0bd871be423b5caf8c24c810717dbf489a
RichyHBM <gitemail@richyhbm.co.uk>
2017-05-21 18:43:29 +0100

    Allow setting image in AppIntro from Iconics

--- app/src/main/java/uk/co/richyhbm/coinbag/activities/WelcomeActivity.kt
+++ app/src/main/java/uk/co/richyhbm/coinbag/activities/WelcomeActivity.kt
@@ -1,27 +1,46 @@
 package uk.co.richyhbm.coinbag.activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import com.github.paolorotolo.appintro.AppIntro
 import com.github.paolorotolo.appintro.AppIntroFragment
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.iconics.IconicsDrawable
 import uk.co.richyhbm.coinbag.R
+import uk.co.richyhbm.coinbag.fragments.WelcomeSlideFragment
 
 
 class WelcomeActivity : AppIntro() {
+    val seenIntro = "seenIntro"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val sharedPrefs = getPreferences(Context.MODE_PRIVATE)
+
+        if (sharedPrefs.getBoolean(seenIntro, false)) {
+            val i = Intent(this@WelcomeActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+
         super.onCreate(savedInstanceState)
 
         val backgroundColor: Int = ContextCompat.getColor(this, R.color.grey_50)
         val textColor : Int = ContextCompat.getColor(this, R.color.light_bg_dark_secondary_text)
         val indicatorButtons : Int = ContextCompat.getColor(this, R.color.grey_700)
 
-        addSlide(AppIntroFragment.newInstance("Coin Bag", "This is an app, 1", R.drawable.coin_bag, backgroundColor, textColor, textColor))
-        addSlide(AppIntroFragment.newInstance("Coin Bag", "This is an app, 2", R.drawable.coin_bag, backgroundColor, textColor, textColor))
-        addSlide(AppIntroFragment.newInstance("Coin Bag", "This is an app, 3", R.drawable.coin_bag, backgroundColor, textColor, textColor))
-        addSlide(AppIntroFragment.newInstance("Coin Bag", "This is an app, 4", R.drawable.coin_bag, backgroundColor, textColor, textColor))
-        addSlide(AppIntroFragment.newInstance("Coin Bag", "This is an app, 5", R.drawable.coin_bag, backgroundColor, textColor, textColor))
+        addSlide(WelcomeSlideFragment.newInstance(
+                getString(R.string.app_name),
+                "A read-only crypto-currency wallet manager to track and receive crypto-currency payments",
+                R.drawable.coin_bag,
+                backgroundColor, textColor, textColor))
+
+        addSlide(WelcomeSlideFragment.newInstance("Coin Bag", "This is an app, 2", R.drawable.coin_bag, backgroundColor, textColor, textColor))
+        addSlide(WelcomeSlideFragment.newInstance("Coin Bag", "This is an app, 3", R.drawable.coin_bag, backgroundColor, textColor, textColor))
+        addSlide(WelcomeSlideFragment.newInstance("Coin Bag", "This is an app, 4", CommunityMaterial.Icon.cmd_folder, backgroundColor, textColor, textColor))
+        addSlide(WelcomeSlideFragment.newInstance("Coin Bag", "This is an app, 5", R.drawable.coin_bag, backgroundColor, textColor, textColor))
 
         setNextArrowColor(indicatorButtons)
         setColorSkipButton(indicatorButtons)
@@ -41,6 +60,11 @@ class WelcomeActivity : AppIntro() {
 
     override fun onDonePressed(currentFragment: Fragment) {
         super.onDonePressed(currentFragment)
+
+        val editor = getPreferences(Context.MODE_PRIVATE).edit()
+        editor.putBoolean(seenIntro, true)
+        editor.apply()
+        
         val i = Intent(this@WelcomeActivity, MainActivity::class.java)
         startActivity(i)
         finish()
---
+++ app/src/main/java/uk/co/richyhbm/coinbag/fragments/WelcomeSlideFragment.kt
@@ -0,0 +1,90 @@
+package uk.co.richyhbm.coinbag.fragments
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Bundle
+import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.github.paolorotolo.appintro.AppIntroBaseFragment
+import com.github.paolorotolo.appintro.AppIntroFragment
+import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.iconics.IconicsDrawable
+import com.mikepenz.iconics.typeface.IIcon
+
+import uk.co.richyhbm.coinbag.R
+
+
+class WelcomeSlideFragment(val icon: IIcon?) : AppIntroBaseFragment() {
+
+
+    companion object {
+        fun newInstance(title: CharSequence, description: CharSequence,
+                        @DrawableRes imageDrawable: Int, @ColorInt bgColor: Int,
+                        @ColorInt titleColor: Int, @ColorInt descColor: Int): WelcomeSlideFragment {
+            val slide = WelcomeSlideFragment(null)
+            val args = Bundle()
+            args.putString(AppIntroBaseFragment.ARG_TITLE, title.toString())
+            args.putString(AppIntroBaseFragment.ARG_TITLE_TYPEFACE, null)
+            args.putString(AppIntroBaseFragment.ARG_DESC, description.toString())
+            args.putString(AppIntroBaseFragment.ARG_DESC_TYPEFACE, null)
+            args.putInt(AppIntroBaseFragment.ARG_DRAWABLE, imageDrawable)
+            args.putInt(AppIntroBaseFragment.ARG_BG_COLOR, bgColor)
+            args.putInt(AppIntroBaseFragment.ARG_TITLE_COLOR, titleColor)
+            args.putInt(AppIntroBaseFragment.ARG_DESC_COLOR, descColor)
+            slide.arguments = args
+
+            return slide
+        }
+
+        fun newInstance(title: CharSequence, description: CharSequence,
+                        imageDrawable: IIcon, @ColorInt bgColor: Int,
+                        @ColorInt titleColor: Int, @ColorInt descColor: Int): WelcomeSlideFragment {
+            val slide = WelcomeSlideFragment(imageDrawable)
+            val args = Bundle()
+            args.putString(AppIntroBaseFragment.ARG_TITLE, title.toString())
+            args.putString(AppIntroBaseFragment.ARG_TITLE_TYPEFACE, null)
+            args.putString(AppIntroBaseFragment.ARG_DESC, description.toString())
+            args.putString(AppIntroBaseFragment.ARG_DESC_TYPEFACE, null)
+            args.putInt(AppIntroBaseFragment.ARG_DRAWABLE, R.drawable.ic_done_white)
+            args.putInt(AppIntroBaseFragment.ARG_BG_COLOR, bgColor)
+            args.putInt(AppIntroBaseFragment.ARG_TITLE_COLOR, titleColor)
+            args.putInt(AppIntroBaseFragment.ARG_DESC_COLOR, descColor)
+            slide.arguments = args
+
+            return slide
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val v =  super.onCreateView(inflater, container, savedInstanceState)
+
+        if(v != null && icon != null){
+            val i = v.findViewById(com.github.paolorotolo.appintro.R.id.image) as ImageView
+            i.setImageDrawable(IconicsDrawable(this.context)
+                    .icon(icon)
+                    .color(Color.parseColor("#000000"))
+                    .sizeDp(180))
+        }
+
+        return v
+    }
+
+    override fun getLayoutId(): Int {
+        return com.github.paolorotolo.appintro.R.layout.fragment_intro
+    }
+}

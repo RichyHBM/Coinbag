5e2f261389240fd26ceaf35d3a9c93a513c352e6
RichyHBM <gitemail@richyhbm.co.uk>
2017-05-21 11:51:42 +0100

    Add FAB button

--- app/src/main/java/uk/co/richyhbm/coinbag/activities/MainActivity.kt
+++ app/src/main/java/uk/co/richyhbm/coinbag/activities/MainActivity.kt
@@ -1,13 +1,19 @@
 package uk.co.richyhbm.coinbag.activities
 
-import android.app.Activity
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import uk.co.richyhbm.coinbag.R
+import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
+import com.mikepenz.iconics.IconicsDrawable
+import uk.co.richyhbm.coinbag.R
+import com.mikepenz.iconics.context.IconicsContextWrapper
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById(R.id.main_fab) as FloatingActionButton
+
+        fab.setImageDrawable(
+                IconicsDrawable(this)
+                .icon(CommunityMaterial.Icon.cmd_plus)
+                .color(ContextCompat.getColor(this, R.color.grey_50))
+                .sizeDp(18))
+
+        fab.setOnClickListener( { view: View ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show()
+        })
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
